5a9dd6bb56c0b4154633f9404cab71caa2d1b91f
catherine <cathsim1971@icloud.com>
2020-10-08 14:24:12 +0100

    nvbar

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,18 @@
+/** @format */
+
+import React from 'react';
+import {Container, Row, Col} from 'react-bootstrap';
+
+const Footer = () => {
+  return <footer>
+  <Container>
+  <Row>
+  <Col classNmae='text-center py-3'>
+  Copyright &copy; ProShop 2020
+  </Col>
+  </Row>
+  </Container>
+  </footer>;
+};
+
+export default Footer;
