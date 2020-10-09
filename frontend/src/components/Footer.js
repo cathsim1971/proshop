6b09b939008d96fd6ba4173ceed48f535e833a3d
catherine <cathsim1971@icloud.com>
2020-10-09 14:57:10 +0100

    products

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,18 +1,18 @@
 /** @format */
 
 import React from 'react';
-import {Container, Row, Col} from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Footer = () => {
-  return <footer>
-  <Container>
-  <Row>
-  <Col classNmae='text-center py-3'>
-  Copyright &copy; ProShop 2020
-  </Col>
-  </Row>
-  </Container>
-  </footer>;
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col classNmae='text-center py-3'>Copyright &copy; ProShop 2020</Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 };
 
 export default Footer;
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,4 @@
 /** @format */
-
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 const Header = () => {
@@ -11,8 +10,14 @@ const Header = () => {
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ml-auto'>
-              <Nav.Link href='/cart'> <i className='fas fa-shopping-cart'></i>  Cart</Nav.Link>
-              <Nav.Link href='/login'> <i className='fas fa-users'> </i> Sign In</Nav.Link>
+              <Nav.Link href='/cart'>
+                {' '}
+                <i className='fas fa-shopping-cart'></i> Cart
+              </Nav.Link>
+              <Nav.Link href='/login'>
+                {' '}
+                <i className='fas fa-users'> </i> Sign In
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
